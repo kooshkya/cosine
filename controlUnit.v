2440876bfc813af0ee8b89ab4e0c59a98e9f7481
Amir Kooshky <kooshkya@gmail.com>
2023-06-26 01:15:14 +0330

    fixed controlUnit

--- controlUnit.v
+++ controlUnit.v
@@ -1,25 +1,30 @@
-module controlUnit(input clk, start, stop, output reg [2 : 0] state);
+module controlUnit(input clk, start, stop, output reg [3 : 0] state);
     initial begin
-        state = 3'b0;
+        state = 4'b0;
     end
 
     always @ (posedge clk) begin
-        if (state == 3'b0) begin    // state: stand by
+        if (state == 4'b0) begin    // state: stand by
             if (start == 1'b1) begin
-                state = 3'b001;
+                state = 4'b001;
             end
-        end else if (state == 3'b1) begin   // state: alert
+        end else if (state == 4'b1) begin   // state: alert
             if (start == 0) begin
-                state = 3'd2;
+                state = 4'd2;
             end
-        end else if (state == 3'd2) begin   // state: startCalculation
-            state = 3'd3;
-        end else if (state == 3'd3) begin   // state: accumulateTerms
-            if (stop == 1'b1)
-                state = 3'd4;
-        end else if (state == 3'd4) begin   // state: calculateDistance
+        end else if (state == 4'd2) begin   // state: startCalculation
+            state = 4'd3;
+        end else if (state == 4'd3) begin   // state: accumulateTerms
+            if (stop == 1'b1) begin
+                state = 4'd4;
+            end else begin
+                state = 4'd5;
+            end
+        end else if (state == 4'd4) begin   // state: calculateDistance
             state = 3'd0;
+        end else if (state == 4'd5) begin   // state: remult
+            state = 4'd3;
         end
     end
-
+    
 endmodule
