5477adbe12d9e456c0e0e130f8de7afe04614df3
Amir Kooshky <kooshkya@gmail.com>
2023-06-25 23:21:58 +0330

    fix wire positions

--- datapath.v
+++ datapath.v
@@ -11,15 +11,21 @@ module datapath(input clk, input [2 : 0] state, input [15:0] vSig, input [15:0] 
     wire [15 : 0] adderResult;
     adder myadder();
 
+    wire [15 : 0] VContent;
+    wire [15 : 0] expressionContent;
+    wire [15 : 0] expressionData;
+    wire expressionAndTermLoad;
+    wire [15 : 0] termData;
+    wire [15 : 0] termContent;
 
 
     wire [15 : 0] multiplierA;
     wire [15 : 0] multiplierB;
     wire [15 : 0] multiplierResult;
-    buffer B1 (.incoming(XSig), .cs(StartCalculation), .outgoing(multiplierA [15 : 0]));
-    buffer B2 (.incoming(XSig), .cs(StartCalculation), .outgoing(multiplierB [15 : 0]));
-    buffer B3 (.incoming(VContent), .cs(CalculateDistance), .outgoing(multiplierA [15 : 0]));
-    buffer B4 (.incoming(expressionContent), .cs(CalculateDistance), .outgoing(multiplierA [15 : 0]));
+    buffer B1 (.incoming(XSig), .cs(StartCalculation), .outgoing(multiplierA));
+    buffer B2 (.incoming(XSig), .cs(StartCalculation), .outgoing(multiplierB));
+    buffer B3 (.incoming(VContent), .cs(CalculateDistance), .outgoing(multiplierA));
+    buffer B4 (.incoming(expressionContent), .cs(CalculateDistance), .outgoing(multiplierA));
     
     multiplier mymultiplier(.result(multiplierResult));
 
@@ -27,21 +33,15 @@ module datapath(input clk, input [2 : 0] state, input [15:0] vSig, input [15:0] 
     assign doneRegisterLoad = CalculateDistance;
     defparam Done.n = 1;
     register Done (.clk(clk), .load(CalculateDistance), .clear(1'b0), .inc(1'b0), .asyncclear(StartCalculation), .data(1'b1), .Q(done));
-    wire [15 : 0] VContent;
     register V (.clk(clk), .load(StartCalculation), .clear(1'b0), .inc(1'b0), .asyncclear(1'b0), .data(vSig), .Q(VContent));
     register X2 (.clk(clk), .load(StartCalculation), .clear(1'b0), .inc(1'b0), .asyncclear(1'b0), .data(multiplierResult));
 
-    wire expressionAndTermLoad;
     or(expressionAndTermLoad, StartCalculation, AccumulateTerms);
-    wire [15 : 0] expressionData;
-    multiplexer exprDataMux (.I0(16'b0), .I1(adderResult [15 : 0]), .select(AccumulateTerms), .out(expressionData));
-    wire [15 : 0] expressionContent;
+    multiplexer exprDataMux (.I0(16'b0), .I1(adderResult), .select(AccumulateTerms), .out(expressionData));
     register expression (.clk(clk), .load(expressionAndTermLoad), .clear(1'b0), .inc(1'b0),
          .asyncclear(1'b0), .data(expressionData), .Q(expressionContent));
 
-    wire [15 : 0] termData;
     multiplexer termDataMux (.I0({5'b1, 11'b0}), .I1(multiplierResult), .select(AccumulateTerms), .out(termData));
-    wire [15 : 0] termContent;
     register term (.clk(clk), .load(expressionAndTermLoad), .clear(1'b0),
          .inc(1'b0), .asyncclear(1'b0), .data(termData), .Q(termContent));
 
